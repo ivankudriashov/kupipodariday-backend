346bfb5c1d0e2a99e093cfd1566d97f2aa50cd55
Ivan Kudriashov <pereyyra@rambler.ru>
2022-10-23 20:29:12 +0300

    Первая сдача

---
+++ src/offers/offers.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
+import { OffersService } from './offers.service';
+import { CreateOfferDto } from './dto/create-offer.dto';
+import { UpdateOfferDto } from './dto/update-offer.dto';
+
+@Controller('offers')
+export class OffersController {
+  constructor(private readonly offersService: OffersService) {}
+
+  @Post()
+  create(@Body() createOfferDto: CreateOfferDto) {
+    return this.offersService.create(createOfferDto);
+  }
+
+  @Get()
+  findAll(@Req() req) {
+    return this.offersService.findAll(req.user.id);
+  }
+
+  @Get(':id')
+  findOne(@Req() req, @Param('id') id: string) {
+    return this.offersService.findOne(+id, req.user.id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto) {
+    return this.offersService.update(+id, updateOfferDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.offersService.remove(+id);
+  }
+}
