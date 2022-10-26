8648d07cdf655f13ecf446b602b1b8948f069a8b
Ivan Kudriashov <pereyyra@rambler.ru>
2022-10-26 14:41:41 +0300

    Третья сдача

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -7,18 +7,21 @@ import {
   Param,
   Delete,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { OffersService } from './offers.service';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { UpdateOfferDto } from './dto/update-offer.dto';
+import { JwtGuard } from 'src/auth/jwt.guard';
 
+@UseGuards(JwtGuard)
 @Controller('offers')
 export class OffersController {
   constructor(private readonly offersService: OffersService) {}
 
   @Post()
-  create(@Body() createOfferDto: CreateOfferDto) {
-    return this.offersService.create(createOfferDto);
+  create(@Req() req, @Body() createOfferDto: CreateOfferDto) {
+    return this.offersService.createOne(createOfferDto, req.user.id);
   }
 
   @Get()
